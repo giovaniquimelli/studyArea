b2a6f68a1c7f9b1d158fe85bbdd6b54b1acfc984
Giovani Quimelli <giovaniquimelli@gmail.com>
2022-02-01 10:35:31 -0300

    config SceneDelegate

---
+++ StudyArea/Controllers/Core Tabs/TabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarViewController.swift
+//  StudyArea
+//
+//  Created by Gio on 01/02/22.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpControllers()
+    }
+    
+    private func setUpControllers() {
+        
+    }
+}
